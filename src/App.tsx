f4efb89ffcb7de40864f7cc300e3d49d36b89c0c
joseph <yeojunjeon42@gmail.com>
2025-09-29 10:59:15 +0900

    file tree sturcture minimal working repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function App() {
+
+    return (
+        <div>
+            <h1>Yeojun Jeon</h1>
+            <p>testing testing testing</p>
+        </div>
+    );
+}
+export default App;
