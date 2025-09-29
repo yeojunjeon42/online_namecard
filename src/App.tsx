98b92aa735cfabcf73ca4046eaef3192d09d952f
joseph <yeojunjeon42@gmail.com>
2025-09-29 11:56:39 +0900

    hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Header from './components/Header.tsx';
+import Hero from './components/Hero.tsx';
+import './styles/styles.css';
 
 function App() {
 
     return (
         <div>
             <Header />
+            <Hero />
             {/* 
             
             <Hero />
---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <div>
+            <h1 className="hero-text">
+                Yeojun Jeon: What the Fuck am I doing with my life?
+            </h1>
+        </div>
+    );
+}
+
+export default Hero;
